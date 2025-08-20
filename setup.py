ba38700c3aa4fda0440ad374b821fca77cdf0e54
rahulchowdary143 <yugeshyogi972@gmail.com>
2025-08-20 16:48:10 +0530

    Mongodb connection was done successfully

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages 
+
+setup(
+    name='src',
+    version="0.0.1",
+    author='Yugesh Madala',
+    author_email='yugeshyogi972@gmail.com',
+    packages=find_packages()
+)
+
